c05c26f760918a8dfebfd76b55eac28c7b6af93b
ergincetn01 <ergincetn01@gmail.com>
2023-09-07 14:39:36 +0300

    demo state mngmt added with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,85 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:flutter_scratch/components/navigation/my_home_page.dart';
+import 'package:flutter_scratch/components/state_management/commmon/theme.dart';
+import 'package:flutter_scratch/components/state_management/models/cart.dart';
+import 'package:flutter_scratch/components/state_management/models/catalog.dart';
+import 'package:flutter_scratch/components/state_management/screens/cart.dart';
+import 'package:flutter_scratch/components/state_management/screens/catalog.dart';
+import 'package:flutter_scratch/components/state_management/screens/login.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+import 'package:window_size/window_size.dart';
+
+void main() {
+  setupWindow();
+  runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Demo');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
+}
+
+GoRouter router() {
+  return GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const MyLogin(),
+      ),
+      GoRoute(
+        path: '/catalog',
+        builder: (context, state) => const MyCatalog(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            builder: (context, state) => const MyCart(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const appTitle = 'Drawer Demo';
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: appTitle,
-      home: MyHomePage(title: appTitle),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        theme: appTheme,
+        routerConfig: router(),
+      ),
     );
   }
 }
